3eea14baa4291ade947b68a5162897045d66fae5
Noah012 <noahpierce185016@gmail.com>
2019-03-12 21:33:08 -0500

    finished

---
+++ Todo.java
@@ -0,0 +1,29 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 3-7-2019
+OU ID_______________: 113484432
+OU EMAIL____________: noah.j.pierce@ou.edu
+
+Tasks:
+1. StackHospital<PatientType>
+2. QueueHospital<PatientType>
+3. HealthyPerson
+4. SickPerson
+5. PriorityQueueHospital<PatientType>
+6. write test classes
+
+Estimated Time (minutes):
+1.25 
+2.25 
+3.20
+4.20
+5.35
+6.60
+
+Actual Time (minutes):
+1.25
+2.22
+3.32
+4.40
+5.64
+*/}
